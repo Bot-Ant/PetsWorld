d5b36e188006bbd1a85e551dd569eeacc4800b63
Antonio <elgladiatore1@live.it>
2022-07-23 16:41:47 +0200

    New commit

--- PetsWorld/src/main/java/controller/ServletModificaPay.java
+++ PetsWorld/src/main/java/controller/ServletModificaPay.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,11 +10,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
 
+import model.beans.Indirizzo;
 import model.beans.MetodoPagamento;
+import model.beans.Utente;
+import model.daoImplementation.UtenteImp;
 import model.daoImplementation.metodoPagamentoImp;
+import model.daoInterface.UtenteDao;
 import model.daoInterface.metodoPagamentoDao;
 
 /**
@@ -37,6 +43,8 @@ public class ServletModificaPay extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
+		HttpSession sessione = request.getSession(true);
+		
 		String id = request.getParameter("id");
 		String Numero = request.getParameter("number");
         String Nome = request.getParameter("owner");
@@ -44,13 +52,18 @@ public class ServletModificaPay extends HttpServlet {
         String Anno = request.getParameter("year");
         String Cvv= request.getParameter("cvv");
        
+        Utente account = new Utente();
+        UtenteDao<SQLException> ut= new UtenteImp(source);
+        ArrayList<Indirizzo> indirizzi= new ArrayList<Indirizzo>();
+        ArrayList<MetodoPagamento> metodiPagamento= new ArrayList<MetodoPagamento>();
         
         int ID = Integer.parseInt(id);
        
+        
         MetodoPagamento nuovo = new MetodoPagamento();
         metodoPagamentoDao<SQLException> dao =  new metodoPagamentoImp(source);
         
-        nuovo.setIdUtente(ID);
+        nuovo.setIdpaga(ID);
         nuovo.setNumero(Numero);
         nuovo.setProprietario(Nome);
         nuovo.setMeseScadenza(Mese);
@@ -58,13 +71,35 @@ public class ServletModificaPay extends HttpServlet {
         nuovo.setCvv(Cvv);
         
 
-        
         try {
             dao.modificapay(nuovo);
         }catch (SQLException throwables) {
             throwables.printStackTrace();
         }
         
+       account = (Utente) sessione.getAttribute("utente");
+       
+        try {
+			account=ut.doRetrieveByKey(account.getEmail());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+        try {
+			indirizzi=ut.setIndirizzi(account.getIdUtente());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        
+        try {
+			metodiPagamento=ut.setMetodiPagamento(account.getIdUtente());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        
+        account.setIndirizziSpedizione(indirizzi);
+        account.setMetodiPagamento(metodiPagamento);
+		sessione.setAttribute("utente", account);
 
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("userAccount.jsp");
 		requestDispatcher.forward(request, response);
