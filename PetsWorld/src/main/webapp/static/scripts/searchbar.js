bb11ee1e4bc11207c87ca676a2b91bd294cd6c5e
Antonio Marcone <a.marcone8@studenti.unisa.it>
2022-07-23 10:51:03 +0200

    Minor changes in product page style and searchbar javascript

--- PetsWorld/src/main/webapp/static/scripts/searchbar.js
+++ PetsWorld/src/main/webapp/static/scripts/searchbar.js
@@ -7,9 +7,9 @@ $(document).ready(function(){
         var expression = new RegExp(searchField, "i");
         $.getJSON('static/datas/products.json', function(data) {
             $.each(data, function(key, value){
-                if (value.name.search(expression) != -1 || value.brand.search(expression) != -1)
+                if (value.name.search(expression) != -1 || value.animal.search(expression) != -1)
                 {
-                    $('#result').append('<li class="list-group-item link-class"><img src="'+value.image+'" class="img-thumbnail" /> '+value.name+' | <span class="text-muted">'+value.brand+'</span></li>');
+                    $('#result').append('<li class="list-group-item link-class"><img src="'+value.image+'" class="img-thumbnail" /> '+value.name+' | <span class="text-muted">'+value.animal+'</span></li>');
                 }
             });   
         });
