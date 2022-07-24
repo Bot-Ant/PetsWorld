83d6fffb74c1f6d2388da55c502b0b370a63232b
adinoolfi <giuseppe.adinolfi.01@gmail.com>
2022-07-24 03:27:50 +0200

    servlet aggiunta prodotto e altro

--- PetsWorld/src/main/java/controller/ServletEliminaPay.java
+++ PetsWorld/src/main/java/controller/ServletEliminaPay.java
@@ -2,20 +2,25 @@ package controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
 
 import model.beans.Indirizzo;
 import model.beans.MetodoPagamento;
+import model.beans.Utente;
 import model.daoImplementation.IndirizzoImp;
+import model.daoImplementation.UtenteImp;
 import model.daoImplementation.metodoPagamentoImp;
 import model.daoInterface.IndirizzoDao;
+import model.daoInterface.UtenteDao;
 import model.daoInterface.metodoPagamentoDao;
 
 /**
@@ -46,6 +51,12 @@ public class ServletEliminaPay extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		HttpSession sessione = request.getSession(true);
+		Utente account = new Utente();
+        ArrayList<Indirizzo> indirizzi= new ArrayList<Indirizzo>();
+        ArrayList<MetodoPagamento> metodiPagamento= new ArrayList<MetodoPagamento>();
+        UtenteDao<SQLException> ut= new UtenteImp(source);
+
 		String intero = request.getParameter("elimina");
 		
 		int ID = Integer.parseInt(intero);
@@ -64,9 +75,32 @@ public class ServletEliminaPay extends HttpServlet {
 			e.printStackTrace();
 		}
 	     
+	     account = (Utente) sessione.getAttribute("utente");
+	       
+	        try {
+				account=ut.doRetrieveByKey(account.getEmail());
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+	        try {
+				indirizzi=ut.setIndirizzi(account.getIdUtente());
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+	        
+	        try {
+				metodiPagamento=ut.setMetodiPagamento(account.getIdUtente());
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+	        
+	        account.setIndirizziSpedizione(indirizzi);
+	        account.setMetodiPagamento(metodiPagamento);
+			sessione.setAttribute("utente", account);
+	     
 	    
-	     request.getRequestDispatcher("userAccount.jsp").forward(request,response);
-	
+	     request.getRequestDispatcher("userPayMethods.jsp").forward(request,response);
 }
 
 }
--- PetsWorld/src/main/java/controller/ServletNewPay.java
+++ PetsWorld/src/main/java/controller/ServletNewPay.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,11 +10,16 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
 
+import model.beans.Indirizzo;
 import model.beans.MetodoPagamento;
+import model.beans.Utente;
+import model.daoImplementation.UtenteImp;
 import model.daoImplementation.metodoPagamentoImp;
+import model.daoInterface.UtenteDao;
 import model.daoInterface.metodoPagamentoDao;
 
 /**
@@ -36,6 +42,12 @@ public class ServletNewPay extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		HttpSession sessione = request.getSession(true);
+		Utente account = new Utente();
+        ArrayList<Indirizzo> indirizzi= new ArrayList<Indirizzo>();
+        ArrayList<MetodoPagamento> metodiPagamento= new ArrayList<MetodoPagamento>();
+        UtenteDao<SQLException> ut= new UtenteImp(source);
+		
 		String id = request.getParameter("id");
 		String Numero = request.getParameter("number");
         String Nome = request.getParameter("owner");
@@ -56,16 +68,37 @@ public class ServletNewPay extends HttpServlet {
         nuovo.setAnnoScadenza(Anno);
         nuovo.setCvv(Cvv);
         
-
-        
         try {
             dao.aggiungi_metodo_pagamento(nuovo);
         }catch (SQLException throwables) {
             throwables.printStackTrace();
         }
         
+        account = (Utente) sessione.getAttribute("utente");
+	       
+        try {
+			account=ut.doRetrieveByKey(account.getEmail());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-		RequestDispatcher requestDispatcher = request.getRequestDispatcher("userAccount.jsp");
+        try {
+			indirizzi=ut.setIndirizzi(account.getIdUtente());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        
+        try {
+			metodiPagamento=ut.setMetodiPagamento(account.getIdUtente());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+        
+        account.setIndirizziSpedizione(indirizzi);
+        account.setMetodiPagamento(metodiPagamento);
+		sessione.setAttribute("utente", account);
+		
+		RequestDispatcher requestDispatcher = request.getRequestDispatcher("userPayMethods.jsp");
 		requestDispatcher.forward(request, response);
 	}
 	/**
